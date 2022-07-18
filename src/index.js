f8c4428edcef9baf211388ffe7d4155c1a9c1250
Ridwan <ridwanediallo99@gmail.com>
2022-07-18 20:31:00 +0000

    Provide store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
-  </Router>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );

b030eae408a418b383b73554e17b08df7024211f
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-19 15:38:00 +0100

    Update rocket structure layout

--- src/Components/Rocket/Rocket.js
+++ src/Components/Rocket/Rocket.js
@@ -1,47 +1,35 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import RocketList from './RocketList';
 import rocket from './rocket.jpg';
 import './Rocket.css';
 
-const rockets = [
-  {
-    id: 1,
-    name: 'Falcon 1',
-    description: 'The Falcom Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    images: rocket,
-  },
-  {
-    id: 2,
-    name: 'Falcon 2',
-    description: 'The tinj huf Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    images: rocket,
-  },
-  {
-    id: 3,
-    name: 'Falcon 3',
-    description: 'in Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    images: rocket,
-  },
-];
+const Rocket = () => {
+  const [rockets] = useState([
+    {
+      id: 1,
+      name: 'Falcon 1',
+      description: 'The Falcom Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      images: rocket,
+    },
+    {
+      id: 2,
+      name: 'Falcon 2',
+      description: 'The tinj huf Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      images: rocket,
+    },
+    {
+      id: 3,
+      name: 'Falcon 3',
+      description: 'in Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      images: rocket,
+    },
+  ]);
 
-const Rocket = () => (
-  rockets.map((rocket) => (
-    <>
-      <div className="container-fluid d-flex my-3 mx-5" key={rocket.id}>
-        <img src={rocket.images} alt="rocket flickr" className="rocket-img" />
-        <div className="content px-3 py-5">
-          <h3>{ rocket.name }</h3>
-          <p>{rocket.description}</p>
-          <Button
-            type="button"
-            variant="primary"
-          >
-            Reserve Rocket
-          </Button>
-        </div>
-      </div>
-    </>
-  ))
-);
+  return (
+    <div>
+      <RocketList rockets={rockets} />
+    </div>
+  );
+};
 
 export default Rocket;
---
+++ src/Components/Rocket/RocketList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes, { Array } from 'prop-types';
+import RocketItem from './RocketItem';
+
+const RocketList = (props) => {
+  const { rockets } = props;
+
+  return (
+    <ul>
+      { rockets.map((rocket) => <RocketItem key={rocket.id} rocket={rocket} />)}
+    </ul>
+  );
+};
+
+RocketList.propTypes = {
+  rockets: PropTypes.instanceOf(Array).isRequired,
+};
+
+export default RocketList;

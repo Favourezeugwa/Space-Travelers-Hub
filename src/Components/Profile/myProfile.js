35e643173865d3bcb9818cb03c8bf85376fca97b
Ridwan <ridwanediallo99@gmail.com>
2022-07-21 09:28:09 +0000

    Add my profile page structure

--- src/Components/Profile/myProfile.js
+++ src/Components/Profile/myProfile.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { ListGroup, Container } from 'react-bootstrap';
 
 const MyProfile = () => (
-  <div>myProfile Here</div>
+  <Container className="d-flex justify-content-center mt-5">
+    <ListGroup className="w-25 me-5">
+      <h2>My Mission</h2>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+    </ListGroup>
+    <ListGroup className="w-25">
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+    </ListGroup>
+  </Container>
 );
 
 export default MyProfile;

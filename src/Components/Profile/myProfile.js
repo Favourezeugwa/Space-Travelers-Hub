6866705c7217b0feb98899e21186ffeda1710c97
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-21 11:58:35 +0100

    add reserved rocket on my profile

--- src/Components/Profile/myProfile.js
+++ src/Components/Profile/myProfile.js
@@ -6,12 +6,16 @@ const MyProfile = () => {
   const missions = useSelector((state) => state.mission);
   const filteredMissions = missions.filter((mission) => mission.canceled === true);
 
+  const rockets = useSelector((state) => state.rocket);
+  const filteredRockets = rockets.filter((rocket) => rocket.reserved === true);
+  console.log(filteredRockets);
+
   return (
     <Container className="d-flex justify-content-center mt-5">
       <ListGroup className="w-25 me-5">
         <h2>My Missions</h2>
         {filteredMissions.length === 0 ? (
-          <p>Please would you like to join the mission with Favour!</p>
+          <p className="text-danger">Please would you like to join the mission with Favour?</p>
         ) : (
           filteredMissions.map((mission) => (
             <ListGroup.Item key={mission.id}>{mission.name}</ListGroup.Item>
@@ -20,7 +24,12 @@ const MyProfile = () => {
       </ListGroup>
       <ListGroup className="w-25">
         <h2>My Rockets</h2>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        {filteredRockets.length === 0 ? (<p className="text-danger">Hey! Kindly pick a rocket.</p>
+        ) : (
+          filteredRockets.map((rocket) => (
+            <ListGroup.Item key={rocket.id}>{rocket.name}</ListGroup.Item>
+          ))
+        )}
       </ListGroup>
     </Container>
   );
